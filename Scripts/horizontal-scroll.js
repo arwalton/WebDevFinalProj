4a58aaf58a420f52c604ac9403f2de105f45db13
arwalton <adamrwalton@gmail.com>
2021-08-27 12:45:47 +0300

    Main elements of index page, started meetups page

--- Scripts/horizontal-scroll.js
+++ Scripts/horizontal-scroll.js
@@ -8,3 +8,36 @@ lists.forEach(elem => {
     const num = elem.children.length;
     elem.style.setProperty('--total', num);
 });
+
+const scrollEle = document.getElementById('meetup-scroll');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollEle.addEventListener('mousedown', (e)=>{
+    isDown = true;
+
+    scrollEle.classList.add('active');
+    startX = e.pageX - scrollEle.offsetLeft;
+    scrollLeft = scrollEle.scrollLeft;
+});
+
+scrollEle.addEventListener('mouseleave', ()=>{
+    isDown = false;
+    scrollEle.classList.remove('active');
+    
+});
+
+scrollEle.addEventListener('mouseup', ()=>{
+    isDown = false;
+    scrollEle.classList.remove('active');
+})
+
+scrollEle.addEventListener('mousemove', (e)=>{
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollEle.offsetLeft;
+    const walk = (x - startX) * 2; //scroll-fast
+    scrollEle.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
--- Scripts/main.js
+++ Scripts/main.js
@@ -6,3 +6,14 @@ const scrollContext = {meetupList: ['box1', 'box2', 'box3', 'box4', 'box5']};
 const scrollHtml = scrollFunc(scrollContext);
 const scrollWrapper = document.getElementById('meetup-scroll');
 scrollWrapper.innerHTML = scrollHtml;
+
+//Compile and insert the handlebars template for random article
+const articleSource = document.getElementById('random-article').innerHTML;
+const articleFunc = Handlebars.compile(articleSource);
+//TODO get actual articles...
+const articleContext = {current: {title: 'Computers Today', 
+content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit Nam viverra ut urna a blandit. Duis condimentum sit amet augue at sollicitudin. Vestibulum ultrices ultricies nisl. Sed imperdiet, mi vitae venenatis gravida, sapien sem luctus est, quis bibendum purus lorem eget ipsum. Morbi pretium scelerisque mi vel molestie. In eu suscipit neque, scelerisque sodales odio. Etiam sit amet nibh eget nibh consectetur fermentum id ac erat.'}};
+
+const articleHtml = articleFunc(articleContext);
+const articleHr = document.getElementById('article-hr');
+articleHr.insertAdjacentHTML('beforebegin', articleHtml);
